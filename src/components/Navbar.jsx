590949c0a826014d81906b04ac1197c1390d89e7
Abhinav Rana <abhinavrana.17r@gmail.com>
2025-06-12 22:15:07 +0530

    A

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../assets/images/logo.png'
+export default function navbar() {
+    return (
+
+
+        <div className='bg-white'>
+
+            <div className='text-3xl flex justify-between items-center'>
+            <img className='h-[200px] w-[200px]' src={Logo} alt="" />
+            
+            </div>
+
+
+
+
+        </div>
+    )
+}
