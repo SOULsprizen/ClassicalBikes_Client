e33694485b83241f480f782330fffafa4f7cd43b
Abhinav Rana <abhinavrana.17r@gmail.com>
2025-06-15 12:04:46 +0530

    djfb

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import Navbar from './components/Navbar/Navbar.jsx'
 import Home from './components/Home/Home.jsx'
 import SignUp from './components/SIgnUp/SignUp.jsx'
 import LogIn from './components/LogIn/LogIn.jsx'
-
+import Bikes from './components/Bikes/Bikes.jsx'
+import About from './components/About/About.jsx'
+import Dealers from './components/Dealer/Dealers.jsx'
+import PartsandAccessories from './components/Parts/PartsandAccessories.jsx'
+import Feedback from './components/Feedback/Feedback.jsx'
+import Shop from './components/Shop/Shop.jsx'
 export default function App() {
   return (
     <BrowserRouter>
@@ -13,6 +18,12 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/log-in' element={<LogIn />} />
+        <Route path='/bikes' element={<Bikes />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dealers' element={<Dealers />} />
+        <Route path='/parts-accessories' element={<PartsandAccessories />} />
+        <Route path='/feedback' element={<Feedback/>} />
+        <Route path='/shop' element={<Shop/>} />
       </Routes>
     </BrowserRouter>
   )
