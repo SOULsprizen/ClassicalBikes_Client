9afffd2ecf2b1a2261e2ad1edee3c720b988f248
Abhinav Rana <abhinavrana.17r@gmail.com>
2025-06-15 19:16:52 +0530

    dev

--- src/components/SIgnUp/SignUp.jsx
+++ src/components/SIgnUp/SignUp.jsx
@@ -42,14 +42,40 @@ export default function SignUp() {
 
 
 {/* right */}
-<div className=''>
+<div className='w-1/2 text-white bg-gradient-to-r from-purple-500 to-blue-500 h-screen'>
   {/* join */}
-     <div className='flex flex-col items-center h-screen gap-4'>
+     <div className='flex flex-col justify-between text-4xl items-center '>
          <ul className='font-extrabold'>VROOOOM!</ul>
-         <ul>
-          Sign Up To Get News 'bout The Latest  Biker Events
-         </ul>
+         <ul> Sign Up To Get News 'bout The Latest  Biker Events  </ul>
      </div>
+
+{/* methods */}
+<div className='flex items-center h-30 w-fit'>
+<ul>Sign Up With Google</ul>
+</div>
+
+
+{/* info */}
+<div className='flex flex-col gap-7'>
+
+<div>
+<input type="text" className='border-2 border-gray-300 rounded-md p-2 w-full' placeholder='Enter your name' />
+</div>
+<div>
+  <input type="email" className='border-2 border-gray-300 rounded-md p-2 w-full' placeholder='Enter your Email'/>
+</div>
+<div>
+  <input type="password" className='border-2 border-gray-300 rounded-md p-2 w-full' placeholder='Enter Password' />
+</div>
+<div>
+  <input type="checkbox" className='border-2 border-gray-300 rounded-md p-2 w-full ' /><span className='flex justify-center'>Save Password</span>
+</div>
+<div className='flex items-center justify-center'>
+  <button type="submit " >Sign Up</button>
+</div>
+</div>
+
+
 
 </div>
 
